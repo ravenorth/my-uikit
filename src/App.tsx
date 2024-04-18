61647d91742df045994f8ca8262d80a0142c4b58
olesya <olesyatiulkina@gmail.com>
2024-04-19 02:17:24 +0300

    Добавила prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            My uikit
-        </div>
-    )
+	return <div className="App">My uikit</div>
 }
 
-export {
-    App,
-}
+export {App}
